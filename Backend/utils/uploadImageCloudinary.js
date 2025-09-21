6f356d4002ec9c28dcd1beca053972d8df4a3d52
ashish02003 <ashishga2003@gmail.com>
2025-09-21 10:45:42 +0530

    my changing files for deplyoing

--- Backend/utils/uploadImageCloudinary.js
+++ Backend/utils/uploadImageCloudinary.js
@@ -4,7 +4,7 @@ import { v2 as cloudinary } from 'cloudinary';
  cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API__SECRET_KEY// Click 'View API Keys' above to copy your API secret
+    api_secret: process.env.CLOUDINARY_API_SECRET_KEY// Click 'View API Keys' above to copy your API secret
 });
  
  const uploadImageClodinary = async(image)=>{
